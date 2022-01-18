4d84a73a6e2a4d874ef735498649fff02e11a638
Dominik Dębowczyk <dominik.debowczyk@getindata.com>
2022-01-18 14:56:34 +0100

    STREAM-277 .gitlab-ci.yml release job

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 """streaming_jupyter_integrations module."""
+import os
+import sys
 from typing import List
 
 from setuptools import find_packages, setup
+from setuptools.command.install import install
+
+__version__ = "0.0.1"
 
 with open("README.md") as f:
     README = f.read()
@@ -17,6 +22,20 @@ def get_requirements(filename: str) -> List[str]:
     return reqs
 
 
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self) -> None:
+        tag = os.getenv('CI_COMMIT_TAG')
+
+        if tag != f"v{__version__}":
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, __version__
+            )
+            sys.exit(info)
+
+
 EXTRA_REQUIRE = {
     "tests": [
         "pytest>=6.2.2, <7.0.0",
@@ -28,7 +47,7 @@ EXTRA_REQUIRE = {
 
 setup(
     name="streaming_jupyter_integrations",
-    version="0.0.1",
+    version=__version__,
     description="JupyterNotebook Flink magics",
     long_description=README,
     long_description_content_type="text/markdown",
