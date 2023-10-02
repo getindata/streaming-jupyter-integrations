0e2f81dd1d6662f4b43eb45136ee11b360c1bf96
Andrzej Dackiewicz <andrzej.dackiewicz@external.merckgroup.com>
2023-10-02 17:06:47 +0200

    Fix

--- streaming_jupyter_integrations/magics.py
+++ streaming_jupyter_integrations/magics.py
@@ -796,7 +796,8 @@ class Integrations(Magics):
     @line_magic
     @magic_arguments()
     @argument("-t", "--type", type=str, default="tree", help="The widget type used to display schemas.")
-    @argument("--parallelism", "-p", type=int, help="Flink parallelism to use when retrieving the table tree.", required=False,
+    @argument("--parallelism", "-p", type=int, help="Flink parallelism to use when retrieving the table tree.",
+              required=False,
               default=5)
     def flink_show_table_tree(self, line: str) -> Any:
         args = parse_argstring(self.flink_show_table_tree, line)
